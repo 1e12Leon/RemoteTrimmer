3e55e15cd3a0d65b7e517ab5d41bb7979bc3d5d9
1481259826 <114893375+1481259826@users.noreply.github.com>
2024-10-10 13:41:14 +0800

    Update train.py

--- train.py
+++ train.py
@@ -17,30 +17,28 @@ def setup_seed(seed):
      torch.cuda.manual_seed_all(seed)
      np.random.seed(seed)
      random.seed(seed)
-# 设置随机数种子
+# random number seed
 setup_seed(66)
 
 data_transform = transforms.Compose(
     [
-        # transforms.Resize(32),  # 缩放图片，长宽比不变，最短边为32像素
-        # transforms.CenterCrop(32),  # 从图片中间切出32*32的图片
-        transforms.ToTensor(),  # 将图片（Image）转换成Tensor,归一化至[0，1]
-        transforms.Normalize(mean=[0.408, 0.380, 0.340], std=[0.116, 0.137, 0.203])  # 正则化操作，标准化至[-1,1],规定均值和标准差
+        transforms.ToTensor(), 
+        transforms.Normalize(mean=[0.408, 0.380, 0.340], std=[0.116, 0.137, 0.203])
     ]
 )
 
 dataset = datasets.ImageFolder(root="datasets/EuroSAT", transform=data_transform)
 
-# 划分训练集和测试集
-train_size = int(0.7 * len(dataset))  # 训练集大小为数据集的70%
+# training set and test set
+train_size = int(0.7 * len(dataset))  
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-# 创建数据加载器
+# Data Loader
 train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-# 创建网络模型
+# network model
 name = "resnet18_SE"
 path = "models/"+name+".pth"
 model = resnet18_SE(10)
@@ -48,32 +46,22 @@ model = resnet18_SE(10)
 if torch.cuda.is_available():
     model = model.cuda()
 
-# 损失函数
+# loss function
 loss_fn = nn.CrossEntropyLoss()
 if torch.cuda.is_available():
     loss_fn = loss_fn.cuda()
 
-# 优化器
-# learning_rate = 0.01
+# Optimizer
 learning_rate = 1e-2
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-# 设置训练网络的一些参数
-# 记录训练次数
+# Set training parameters
+# Record training times
 total_train_step = 0
-# 记录测试次数
+# Record testing times
 total_test_step = 0
-# 训练的轮数
+# Number of training epochs
 epoch = 160
-# L1正则化系数
-s = 0.000
-# L1正则化
-L1 = False
-def updateBN():
-    for m in model.modules():
-        if isinstance(m, nn.BatchNorm2d):
-            m.weight.grad.data.add_(s*torch.sign(m.weight.data))
-
             
 def train():
     model.train()
@@ -87,21 +75,20 @@ def train():
         output = model(imgs)
         loss = loss_fn(output, targets)
         
-        # 优化器优化模型
+        # Optimizer
         optimizer.zero_grad()
         loss.backward()
-        if L1: updateBN()
         optimizer.step()
         
-        # 记录训练次数
+        # Record training times
         total_train_step = total_train_step + 1
         
         if total_train_step % 100 == 0:
-            print("训练次数：{}, Loss：{}".format(total_train_step, loss.item()))
+            print("training times：{}, Loss：{}".format(total_train_step, loss.item()))
             # writer.add_scalar("train_loss", loss.item(), total_train_step)
 
 def test():
-    # 测试步骤开始
+    # Test step starts
     model.eval()
     total_test_loss = 0
     total_accuracy = 0
@@ -116,15 +103,15 @@ def test():
             loss = loss_fn(outputs, targets)
             total_test_loss = total_test_loss + loss.item()
             
-            # 准确数
+            # accuracy
             accuracy = (outputs.argmax(1) == targets).sum()
             total_accuracy = total_accuracy + accuracy
             
-    print("整体测试集上的Loss：{}".format(total_test_loss))
-    print("整体测试集上的正确率{}".format(total_accuracy/float(len(test_dataloader.dataset))))
+    print("Loss on the whole test set：{}".format(total_test_loss))
+    print("Accuracy on the whole test set：{}".format(total_accuracy/float(len(test_dataloader.dataset))))
     return total_accuracy / float(len(test_dataloader.dataset))
 
-# 保存模型数据
+# Save model data
 path2 = "model_save/"+name+"_train_best.pth.tar"
 def save_checkpoint(state, is_best, filename='temp_model/train.pth.tar'):
     torch.save(state, filename)
@@ -135,7 +122,7 @@ if __name__ == '__main__':
     # freeze_support()
     best_prec1 = 0.
     for i in range(0, epoch):
-        print("-----第{}轮训练开始-----".format(i+1))
+        print("-----The {}th round of training begins-----".format(i+1))
         if i in [epoch*0.5,epoch*0.75]:
             for param_group in optimizer.param_groups:
                 param_group['lr'] *= 0.1
